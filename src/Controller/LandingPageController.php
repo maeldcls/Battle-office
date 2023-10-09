d9fafca67297e6872db6206766d672dba0ab9074
maeldcls <maelduclos03@gmail.com>
2023-10-09 16:03:57 +0200

    first commit

---
+++ src/Controller/LandingPageController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Form;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LandingPageController extends AbstractController
+{
+    /**
+     * @Route("/", name="landing_page")
+     * @throws \Exception
+     */
+    #[Route('/',name:'landing_page')]
+    public function index(Request $request)
+    {
+        //Your code here
+
+        return $this->render('landing_page/index_new.html.twig', [
+
+        ]);
+    }
+    /**
+     * @Route("/confirmation", name="confirmation")
+     */
+    #[Route('/confirmation',name:'confirmation')]
+    public function confirmation()
+    {
+        return $this->render('landing_page/confirmation.html.twig', [
+
+        ]);
+    }
+}
